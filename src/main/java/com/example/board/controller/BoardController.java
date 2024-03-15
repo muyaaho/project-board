6725b30c32dbdec66f0155e5738b7ea7464269a6
muyaaho <skanthsu715@gmail.com>
2024-03-15 20:44:16 +0900

    Feat: 폼 데이터 받기

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.example.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
@@ -9,5 +10,10 @@ public class BoardController {
     @GetMapping("/board/new")
     public String newArticleForm() {
         return "board/new";
+    }
+
+    @PostMapping("/board/create")
+    public String createArticle() {
+        return "";
     }
 }
