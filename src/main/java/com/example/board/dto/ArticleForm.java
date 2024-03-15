257a1b1aa2d9208ee99dfa50112683d0ab82588d
muyaaho <skanthsu715@gmail.com>
2024-03-15 20:48:18 +0900

    Feat: DTO 생성

---
+++ src/main/java/com/example/board/dto/ArticleForm.java
@@ -0,0 +1,11 @@
+package com.example.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class ArticleForm {
+    private String title;
+    private String content;
+}
