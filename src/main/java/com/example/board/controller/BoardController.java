55c20f679483403411dbb8c10245f15f18098324
muyaaho <skanthsu715@gmail.com>
2024-03-15 21:27:27 +0900

    Feat: DTO를 데이터베이스에 저장하는 기능

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -1,7 +1,10 @@
 package com.example.board.controller;
 
 import com.example.board.dto.ArticleForm;
+import com.example.board.entity.Article;
+import com.example.board.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Slf4j
 @Controller
 public class BoardController {
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @GetMapping("/board/new")
     public String newArticleForm() {
@@ -18,6 +23,12 @@ public class BoardController {
     @PostMapping("/board/create")
     public String createArticle(ArticleForm form) {
         log.info(form.toString());
+
+        Article article = form.toEntity();
+        log.info(article.toString());
+
+        Article saved = articleRepository.save(article);
+        log.info(saved.toString());
         return "";
     }
 }
