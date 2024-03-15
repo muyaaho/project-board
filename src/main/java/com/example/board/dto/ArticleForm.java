8ef73f59ef96cf7828d77fa3f8f33e0b06928da4
muyaaho <skanthsu715@gmail.com>
2024-03-15 21:20:16 +0900

    Feat: DTO를 엔티티로 변환

--- src/main/java/com/example/board/dto/ArticleForm.java
+++ src/main/java/com/example/board/dto/ArticleForm.java
@@ -1,12 +1,18 @@
 package com.example.board.dto;
 
+import com.example.board.entity.Article;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
 @ToString
 public class ArticleForm {
+    private Long id;
     private String title;
     private String user;
     private String content;
+
+    public Article toEntity() {
+        return new Article(id, title, user, content);
+    }
 }
