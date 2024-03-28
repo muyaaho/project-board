241ceca51c0a1fad1ead698cdacb00352d0199d7
muyaaho <skanthsu715@gmail.com>
2024-03-28 20:16:06 +0900

    Feat: 컨트롤러 생성(댓글 삭제, REST API) # 17

--- src/main/java/com/example/board/api/CommentApiController.java
+++ src/main/java/com/example/board/api/CommentApiController.java
@@ -3,9 +3,11 @@ package com.example.board.api;
 import com.example.board.dto.CommentDto;
 import com.example.board.service.CommentService;
 import java.util.List;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,4 +44,9 @@ public class CommentApiController {
     }
 
     // 4. 댓글 삭제
+    @DeleteMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> delete(@PathVariable Long id) {
+        CommentDto deletedDto = commentService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body(deletedDto);
+    }
 }
